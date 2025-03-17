737fbf35c0609a7bc325f342e9975e0a048b458c
“Vasia <vpupkin@mail.com>
2025-03-17 11:11:59 +0200

    first commit

--- src/main/java/BasicAlgSort.java
+++ src/main/java/BasicAlgSort.java
@@ -1,102 +1,124 @@
 import java.util.Arrays;
+public class BasicAlgSort<T extends Comparable<? super T>> {
+    private int left;
+    private int right;
+    private T[] array;
+    private boolean sortOrderingFlag;
 
-import static java.util.Collections.swap;
-
-public class BasicAlgSort<T extends Comparable<? super T>>{
-     private int left;
-     private int right;
-     private T[] array;
-     private boolean sortOrderingFlag;
-
-     public BasicAlgSort(T[] array,boolean sortOrderingFlag){
-         this.sortOrderingFlag=sortOrderingFlag;
-         this.array=Arrays.copyOf(array, array.length);
-     }
-     public BasicAlgSort(int left, int right, T[] array) {
-         this.left = left;
-         this.right = right;
-         this.array = Arrays.copyOfRange(array,left,right);
-     }
-     public T[] getArray() {
-         return array;
-     }
-     public void bubbleSortAsc(){
-         boolean flagForInteration=true;
-         while (flagForInteration){
-             flagForInteration=false;
-             for (int i =1; i<array.length; i++){
-                 if (sortOrderingFlag) {
-                     if (array[i].compareTo(array[i - 1]) > 0) {
-                         swap(i, i - 1);
-                         if (!flagForInteration) {
-                             flagForInteration = true;
-                         }
-                     }else {
-                         if (array[i - 1].compareTo(array[i]) < 0) {
-                             swap(i, i - 1);
-                             if (!flagForInteration) {
-                                 flagForInteration = true;
-                             }
-                         }
-                     }
-                 }
-             }
-         }
-     }
-
-    private void swap(int leftIndex, int rightIndex) {
-         T temp=array[leftIndex];
-         array[leftIndex]=array[rightIndex];
-         array[rightIndex]=temp;
+    public BasicAlgSort(T[]array,boolean sortOrderingFlag){
+        this.array=Arrays.copyOf(array,array.length);
+        this.sortOrderingFlag = sortOrderingFlag;
     }
-    public void selectionAsc(){
-         for (int i=0; i<array.length; i++){
-             int minIndex=i;
-             T min =array[i];
-             for (int j=i+1; j<array.length; j++){
-                 if (array[j].compareTo(min)<0){
-                     min=array[j];
-                     minIndex=j;
-                 }
-             }
-             if (i!=minIndex) swap(i, minIndex);
-         }
-    }
-    public void insertionSortWithinearSearchAsc(){
-         for (int i=1; i<array.length; i++){
-             T key = array[i];
-             int j=i;
-             for (;j>0;j--){
-                 if (key.compareTo(array[j-1])<0){
-                     array[j]=array[j-1];
-                 }else {
-                     break;
-                 }
-             }
-             array[j]=key;
-         }
-    }
-    public void insertSortWithBinarySearchAsc(){
-         for (int i=1; i<array.length; i++){
-             T key = array[i];
-             int leftIndex=0;
-             int rightIndex=i-1;
-             if(key.compareTo(array[i-1])<0){
-                 while (leftIndex<rightIndex){
-                     int serIndex = (rightIndex + leftIndex)/2;
-                     if (key.compareTo(array[serIndex])<0){
-                         rightIndex=serIndex;
-                     }else {
-                         leftIndex=serIndex+1;
 
-                     }
-                 }
-                 for (int j=i; j>leftIndex;j--){
-                     array[j]=array[j-1];
-                 }
-                 array[leftIndex]=key;
-             }
-         }
+    public BasicAlgSort(int left, int right, T[] array) {
+        this.left = left;
+        this.right = right;
+        this.array = Arrays.copyOfRange(array,left,right);
+    }
+
+    public void bubbleSortAsc() {
+        boolean flagForInteration = true;
+        while (flagForInteration) {
+            flagForInteration = false;
+            for (int i = 1; i < array.length; i++) {
+                if (sortOrderingFlag) {  // Ascending order
+                    if (array[i].compareTo(array[i - 1]) < 0) {
+                        swap(i, i - 1);
+                        flagForInteration = true;
+                    }
+                } else {  // Descending order
+                    if (array[i].compareTo(array[i - 1]) > 0) {
+                        swap(i, i - 1);
+                        flagForInteration = true;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void swap(int leftIndex, int righIndex) {
+        T temp = array[leftIndex];
+        array[leftIndex] = array[righIndex];
+        array[righIndex] = temp;
+    }
+
+
+    public void selectSortAsc() {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            T min = array[i];
+            for (int j = i + 1; j < array.length; j++) {
+                if (sortOrderingFlag) {  // Ascending order
+                    if (array[j].compareTo(min) < 0) {
+                        min = array[j];
+                        minIndex = j;
+                    }
+                } else {  // Descending order
+                    if (array[j].compareTo(min) > 0) {
+                        min = array[j];
+                        minIndex = j;
+                    }
+                }
+            }
+            if (i != minIndex) swap(i, minIndex);
+        }
+    }
+
+
+    public void insertionSortWithLinearSearchAsc() {
+        for (int i = 1; i < array.length; i++) {
+            T key = array[i];
+            int j = i;
+            for (; j > 0; j--) {
+                if (sortOrderingFlag) {  // Ascending order
+                    if (key.compareTo(array[j - 1]) < 0) {
+                        array[j] = array[j - 1];
+                    } else {
+                        break;
+                    }
+                } else {  // Descending order
+                    if (key.compareTo(array[j - 1]) > 0) {
+                        array[j] = array[j - 1];
+                    } else {
+                        break;
+                    }
+                }
+            }
+            array[j] = key;
+        }
+    }
+
+
+    public void insertionSortWithBinarySearchAsc() {
+        for (int i = 1; i < array.length; i++) {
+            T key = array[i];
+            int leftIndex = 0;
+            int rightIndex = i - 1;
+
+            if (key.compareTo(array[i - 1]) < 0) {
+                while (leftIndex <= rightIndex) {
+                    int midIndex = (rightIndex + leftIndex) / 2;
+                    if (key.compareTo(array[midIndex]) < 0) {
+                        rightIndex = midIndex - 1;
+                    } else {
+                        leftIndex = midIndex + 1;
+                    }
+                }
+
+
+                for (int j = i; j > leftIndex; j--) {
+                    array[j] = array[j - 1];
+                }
+                array[leftIndex] = key;
+            }
+        }
+    }
+
+
+
+    public T[] getArray() {
+        return array;
     }
 
 
--- src/test/java/BasicAlgSortTest.java
+++ src/test/java/BasicAlgSortTest.java
@@ -2,57 +2,48 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
-import static org.assertj.core.api.Assertions.assertThat;
-class BasicAlgSortTest{
+import static org.assertj.core.api.Assertions.*;
+
+class BasicAlgSortTest {
     @Test
-    public void testBubbleSortWhenArraysWithIntegerTypesAndRangeEqualsLength() {
-        Integer[] sourceArray=new Integer[]{5,4,10,2,1};
-        Integer[] destArray= new Integer[]{1,2,4,5,10};
-        BasicAlgSort<Integer>integerBasicAlgSort=new BasicAlgSort<>(sourceArray,true);
+    public void testBubbleSortWhenArraysWithIntegerTypesAndRAngeEqualsLength(){
+        Integer[] sourceArray= new Integer[]{5,4,10,2,1};
+        Integer [] destArray=new Integer[]{1,2,4,5,10};
+        BasicAlgSort<Integer> integerBasicAlgSort=new BasicAlgSort<>(sourceArray, true);
         integerBasicAlgSort.bubbleSortAsc();
         assertThat(integerBasicAlgSort.getArray()).isEqualTo(destArray);
     }
     @Test
-    public void testBubbleSortWhenArraysWithIntegerTypesAndRangeEqualsLength2() {
-        Integer[] sourceArray=new Integer[]{5,4,10,2,1};
-        Integer[] destArray= new Integer[]{10,5,4,2,1};
-        BasicAlgSort<Integer>integerBasicAlgSort=new BasicAlgSort<>(sourceArray,false);
-        integerBasicAlgSort.bubbleSortAsc();
+    public void testSelectionSortWhenArraysWithIntegerTypesAndRAngeEqualsLength(){
+        Integer[] sourceArray= new Integer[]{5,4,10,2,1};
+        Integer [] destArray=new Integer[]{1,2,4,5,10};
+        BasicAlgSort<Integer> integerBasicAlgSort=new BasicAlgSort<>(sourceArray, true);
+        integerBasicAlgSort.selectSortAsc();
         assertThat(integerBasicAlgSort.getArray()).isEqualTo(destArray);
     }
-
     @Test
-    public void testSelectionSortWhenArraysWithIntegerTypesAndRangeEqualsLength() {
-        Integer[] sourceArray=new Integer[]{5,4,10,2,1};
-        Integer[] destArray= new Integer[]{1,2,4,5,10};
-        BasicAlgSort<Integer>integerBasicAlgSort=new BasicAlgSort<>(sourceArray,true);
-        integerBasicAlgSort.bubbleSortAsc();
-        assertThat(integerBasicAlgSort.getArray()).isEqualTo(destArray);
-
-    }
-
-    @Test
-    public void testInsertionSortWhenArraysWithIntegerTypesAndRangeEqualsLength() {
-        Integer[] sourceArray=new Integer[]{5,4,10,2,1,8,-5,0};
-        Integer[] destArray= new Integer[]{-5,0,1,2,4,5,8,10};
-        BasicAlgSort<Integer>integerBasicAlgSort=new BasicAlgSort<>(sourceArray, true);
-        integerBasicAlgSort.insertionSortWithinearSearchAsc();
+    public void testInsertionSortWhenArraysWithIntegerTypesAndRAngeEqualsLength(){
+        Integer[] sourceArray= new Integer[]{5,4,10,2,1,8,-5,0};
+        Integer [] destArray=new Integer[]{-5,0,1,2,4,5,8,10};
+        BasicAlgSort<Integer> integerBasicAlgSort=new BasicAlgSort<>(sourceArray, true);
+        integerBasicAlgSort.insertionSortWithLinearSearchAsc();
         assertThat(integerBasicAlgSort.getArray()).isEqualTo(destArray);
     }
-
     @Test
-    public void testInsertionSortWithBinarySearchWhenArraysWithIntegerTypesAndRangeEqualsLength() {
-        Integer[] sourceArray=new Integer[]{5,4,10,2,1,8,-5,0};
-        Integer[] destArray= new Integer[]{-5,0,1,2,4,5,8,10};
-        BasicAlgSort<Integer>integerBasicAlgSort=new BasicAlgSort<>(sourceArray, true);
-        integerBasicAlgSort.insertionSortWithinearSearchAsc();
+    public void testInsertionSortWithBinarySearchWhenArraysWithIntegerTypesAndRAngeEqualsLength(){
+        Integer[] sourceArray= new Integer[]{5,4,10,2,1,8,-5,0};
+        Integer [] destArray=new Integer[]{-5,0,1,2,4,5,8,10};
+        BasicAlgSort<Integer> integerBasicAlgSort=new BasicAlgSort<>(sourceArray, true);
+        integerBasicAlgSort.insertionSortWithBinarySearchAsc();
         assertThat(integerBasicAlgSort.getArray()).isEqualTo(destArray);
     }
     public static Integer[] getRandomInteger(int n){
-        Integer[] sourceArray = new Integer[n];
-        for (int i=0; i<n; i++){
+        Integer[] sourceArray=new Integer[n];
+        for (int i = 0; i < n; i++) {
             sourceArray[i]=new Random().nextInt(200)-100;
         }
         return sourceArray;
     }
+
+
 }
